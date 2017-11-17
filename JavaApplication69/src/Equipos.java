adaea95212a17e4d9a5f875d083899b217c86c74
Richard Padgett <richard.padgett@hotmail.com>
2017-11-17 13:26:38 -0600

    Creacion de clase jugadores y equipos

---
+++ JavaApplication69/src/Equipos.java
@@ -0,0 +1,67 @@
+
+import java.util.ArrayList;
+
+
+public class Equipos {
+    String nombre;
+    int presupuesto;
+    int copasgan;
+    String estadio;
+    ArrayList<Jugadores> jug = new ArrayList();
+
+    public Equipos() {
+    }
+
+    public Equipos(String nombre, int presupuesto, int copasgan, String estadio) {
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+        this.copasgan = copasgan;
+        this.estadio = estadio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(int presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public int getCopasgan() {
+        return copasgan;
+    }
+
+    public void setCopasgan(int copasgan) {
+        this.copasgan = copasgan;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public ArrayList<Jugadores> getJug() {
+        return jug;
+    }
+
+    public void setJug(ArrayList<Jugadores> jug) {
+        this.jug = jug;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipos{" + "nombre=" + nombre + ", presupuesto=" + presupuesto + ", copasgan=" + copasgan + ", estadio=" + estadio + ", jug=" + jug + '}';
+    }
+    
+}
---
+++ JavaApplication69/src/Jugadores.java
@@ -0,0 +1,87 @@
+
+public class Jugadores {
+    String nombre;
+    int precio;
+    String pos;
+    String disponibilidad;
+    double habilidad;
+    double tecnica;
+    double resistencia;
+
+    public Jugadores() {
+    }
+
+    public Jugadores(String nombre, int precio, String pos, String disponibilidad, double habilidad, double tecnica, double resistencia) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.pos = pos;
+        this.disponibilidad = disponibilidad;
+        this.habilidad = habilidad;
+        this.tecnica = tecnica;
+        this.resistencia = resistencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getPos() {
+        return pos;
+    }
+
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public double getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(double habilidad) {
+        this.habilidad = habilidad;
+    }
+
+    public double getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(double tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public double getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(double resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugadores{" + "nombre=" + nombre + ", precio=" + precio + ", pos=" + pos + ", disponibilidad=" + disponibilidad + ", habilidad=" + habilidad + ", tecnica=" + tecnica + ", resistencia=" + resistencia + '}';
+    }
+       
+    
+    
+}
